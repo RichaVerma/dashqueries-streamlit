abbb393927482066cb2b38662342972304abd348
RichaVerma <richa.verma8792@gmail.com>
2023-03-15 23:07:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,17 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+import json
 
 st.title('Dashqueries: Analyzing Opportunities')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://github.com/RichaVerma/dashqueries-streamlit/blob/main/data.json')
 
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    print(data)
-#     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+def load_data():
+    data = json.load(DATA_URL)
     return data
 
-data = load_data(100)
+data = load_data()
 
 st.write(data)
