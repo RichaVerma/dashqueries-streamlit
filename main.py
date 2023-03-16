ef0185ad7ee574cc5acdcd6b5e70c7014977b14a
RichaVerma <richa.verma8792@gmail.com>
2023-03-15 23:02:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,11 @@ import numpy as np
 st.title('Dashqueries: Analyzing Opportunities')
 
 DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATA_URL = ('https://github.com/RichaVerma/dashqueries-streamlit/blob/main/data.json')
 
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+#     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
